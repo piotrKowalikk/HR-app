60f35ab0ac404c37229e59a0fdc1b0fdb8c88050
Piotr Kowalik <kowalikk.piotr@gmail.com>
2019-11-27 00:49:05 +0100

    view templates

---
+++ WebApp-OpenIDConnect-DotNet/Controllers/JobApplicationsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HR.DataAccess.Models;
+
+namespace Web.Controllers
+{
+    public class JobApplicationsController : Controller
+    {
+        private readonly HR_ProjectContext _context;
+
+        public JobApplicationsController(HR_ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: JobApplications
+        public async Task<IActionResult> Index()
+        {
+            var hR_ProjectContext = _context.JobApplications.Include(j => j.JobOffer).Include(j => j.User);
+            return View(await hR_ProjectContext.ToListAsync());
+        }
+
+        // GET: JobApplications/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobApplication = await _context.JobApplications
+                .Include(j => j.JobOffer)
+                .Include(j => j.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobApplication == null)
+            {
+                return NotFound();
+            }
+
+            return View(jobApplication);
+        }
+
+        // GET: JobApplications/Create
+        public IActionResult Create()
+        {
+            ViewData["JobOfferId"] = new SelectList(_context.Offers, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: JobApplications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,JobOfferId,UserId,FirstName,LastName,PhoneNumber,EmailAddress,ContactAgreement,CVurl")] JobApplication jobApplication)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jobApplication);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobOfferId"] = new SelectList(_context.Offers, "Id", "Id", jobApplication.JobOfferId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", jobApplication.UserId);
+            return View(jobApplication);
+        }
+
+        // GET: JobApplications/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobApplication = await _context.JobApplications.FindAsync(id);
+            if (jobApplication == null)
+            {
+                return NotFound();
+            }
+            ViewData["JobOfferId"] = new SelectList(_context.Offers, "Id", "Id", jobApplication.JobOfferId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", jobApplication.UserId);
+            return View(jobApplication);
+        }
+
+        // POST: JobApplications/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,JobOfferId,UserId,FirstName,LastName,PhoneNumber,EmailAddress,ContactAgreement,CVurl")] JobApplication jobApplication)
+        {
+            if (id != jobApplication.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jobApplication);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JobApplicationExists(jobApplication.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobOfferId"] = new SelectList(_context.Offers, "Id", "Id", jobApplication.JobOfferId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", jobApplication.UserId);
+            return View(jobApplication);
+        }
+
+        // GET: JobApplications/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobApplication = await _context.JobApplications
+                .Include(j => j.JobOffer)
+                .Include(j => j.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobApplication == null)
+            {
+                return NotFound();
+            }
+
+            return View(jobApplication);
+        }
+
+        // POST: JobApplications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jobApplication = await _context.JobApplications.FindAsync(id);
+            _context.JobApplications.Remove(jobApplication);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JobApplicationExists(int id)
+        {
+            return _context.JobApplications.Any(e => e.Id == id);
+        }
+    }
+}
