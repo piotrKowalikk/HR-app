fe87e6f0aa8949333b43f654c7ddcf28e7504915
Piotr Kowalik <kowalikk.piotr@gmail.com>
2019-12-02 18:32:34 +0100

    delete user

--- Web/Controllers/UsersController.cs
+++ Web/Controllers/UsersController.cs
@@ -10,16 +10,37 @@ namespace Web.Controllers
 {
     public class UsersController : Controller
     {
-        IUserService<User> userService;
-        public UsersController(IUserService<User> service)
+        HR_ProjectContext _context;
+        public UsersController(HR_ProjectContext context)
         {
-            userService= service;
+            _context= context;
+        }
+
+        // GET: Users/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company =  _context.Users
+                .FirstOrDefault(m => m.Id == id);
+
+            _context.Users.Remove(company);
+            await _context.SaveChangesAsync();
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Index()
         {
-
-            return View(userService.SelectAll());
+            return View(_context.Users.Select(x=>x));
         }
     }
 }
